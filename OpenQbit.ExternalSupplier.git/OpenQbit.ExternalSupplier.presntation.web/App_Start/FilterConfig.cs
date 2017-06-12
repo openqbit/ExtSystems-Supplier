976f7d5a89903dbf960483fe85db646e5911daab
ranulsachin <ranulsachinchathuranga@gmail.com>
2017-06-11 22:55:38 -0700

    01

---
+++ OpenQbit.ExternalSupplier.git/OpenQbit.ExternalSupplier.presntation.web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OpenQbit.ExternalSupplier.presntation.web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OpenQbit.ExternalSupplier.git/OpenQbit.ExternalSupplier.presntation.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQbit.ExternalSupplier.presntation.web.Startup))]
+namespace OpenQbit.ExternalSupplier.presntation.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
